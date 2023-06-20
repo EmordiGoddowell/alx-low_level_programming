f4a391c399f99d396ab8516bc272ae9fed40195d
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-06-20 16:24:44 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@
 void print_alphabet_x10(void)
 	{
 		int i;
-		int j;
+		char c;
 
 		for (i = 1; i < 10; i++)
 		{
-			for (j = 97; i <= 122; j++)
+			for (c = 'a'; c <= 'z'; c++)
 			{
-				_putchar(j);
+				_putchar(c);
 			}
 			_putchar('\n');
 		}
