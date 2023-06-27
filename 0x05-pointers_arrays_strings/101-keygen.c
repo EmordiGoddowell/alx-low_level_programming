146a9d983f3a726a8bd3297e636799aaff49dc74
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-06-26 19:53:14 -0700

    password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * generate_password: generates password 94 + 33
+ */
+#define PASSWORD_LENGTH 10
+
+char *generate_password()
+{
+	char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));
+		if (password == NULL)
+			{
+			fprintf(stderr, "Memory allocation failed.\n");
+				exit(1);
+				}
+
+			srand(time(NULL));
+			for (int i = 0; i < PASSWORD_LENGTH; i++)
+					{
+			password[i] = rand() % 94 + 33;
+					}
+				password[PASSWORD_LENGTH] = '\0';
+				return (password);
+}
 
 int main(void)
 {
-	char password[] = "GoddowellEmordi";
+		char *password = generate_password();
 
-	printf("Generated password: %s\n", password);
+		printf("%s\n", password);
+		free(password);
 
-	return (0);
+		return (0);
 }
