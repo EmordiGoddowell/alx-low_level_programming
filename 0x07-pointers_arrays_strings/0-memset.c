d27c11d50dc3a2678951b0fad994c315adfdf8cf
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-07-03 07:24:00 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,9 @@
 * @b: The constant byte to be used to fill the memory area.
 * @n: The number of bytes to be filled.
 *
-* return: The pointer to the memory that was filled.
+* Return: The pointer to the memory that was filled.
+* This pointer can be used to continue working with the memory after
+* it has been filled.
 */
 
 char *_memset(char *s, char b, unsigned int n)
