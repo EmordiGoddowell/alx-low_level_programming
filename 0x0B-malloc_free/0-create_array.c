cc7276b8b19364144f537f1f742191f5b0a9d94b
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-07-11 00:10:23 -0700

    0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
  * create_array - creates an array of size and assign char c
  * @size: size of array
  * @c: char to assign
- * 
+ *
  * Return: pointer to array, NULL if fail
  */
 char *create_array(unsigned int size, char c)
