abeb81b2e3f7b3b7c3ce10375bfb72ac4ccb8962
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-10-02 13:05:07 -0700

    libmy

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point which takes no arguement and returns an integer
+ *
+ * Return: 0 for a succcessful execution.
+**/
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
