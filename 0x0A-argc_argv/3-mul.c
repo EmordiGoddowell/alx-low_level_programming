d28b30a57e128a92b06fbd1f1bab4dae61ba160b
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-10-02 19:15:16 -0700

    arguement mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry proin for this program.
+ * @argc: arguement count.
+ * @argv: argurment vector.
+ *
+ * Return: error if argc is equals to 0
+ * and 1 for a successful execution.
+**/
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (argc == 3)
+	{
+		int n1 = atoi(argv[1]);
+		int n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	return (0);
+}
