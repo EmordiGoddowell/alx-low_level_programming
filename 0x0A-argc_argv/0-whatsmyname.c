003c3b5db8948f4ad571b3177f1e637a0cf08ea7
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-10-02 17:05:53 -0700

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program to ve executed.
+ * @argc: the arguement count.
+ * @argv: the arguement vector.
+ *
+ * Return: 0 for a successful execution.
+**/
+
+int main(int argc, char* argv[])
+{
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
