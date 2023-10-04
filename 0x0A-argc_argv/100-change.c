f7e2f6b3367b95ce0ac0e6ded9244233a3e59374
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-10-03 19:40:28 -0700

    change in cents

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,15 +12,17 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	int cents = atoi(argv[1]);
+	int cents;
 	int num_coins = 0;
 	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc != 2)
+	if (argc <= 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 	{
