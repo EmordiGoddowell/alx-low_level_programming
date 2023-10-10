408eaf1b2dfa220383cd3e80b5af55830f7ef38f
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-10-10 16:37:31 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a structure representing a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: Longer description
+**/
+
 struct dog
 {
 	char *name;
