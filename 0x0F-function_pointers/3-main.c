5a2afe892cc1144403ebf07b21afa44bf07468db
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-10-12 14:46:15 -0700

    calc_num

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -37,8 +37,8 @@ int main(int argc, char *argv[])
 
 	if ((*operator == '/' || *operator == '%') && num2 == 0)
 	{
-	       printf("Error\n");
-	       return (100);
+		printf("Error\n");
+		return (100);
 	}
 
 	printf("%d\n", operation(num1, num2));
